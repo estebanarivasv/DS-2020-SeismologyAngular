c2e214f32cea1ad3071371fa7d259d1711a2e0a7
Esteban Rivas <estebanarivasv@gmail.com>
2020-09-03 16:01:12 -0300

    Creating new branch

--- seismology-project/api/main/resources/Seism.py
+++ seismology-project/api/main/resources/Seism.py
@@ -54,51 +54,6 @@ class VerifiedSeisms(Resource):
         query, pagination = pag.pagination()
         return seisms_schema.dump(query.all())
 
-        """
-        page_num = 1
-        elem_per_page = 25
-        raise_error = True
-        max_elem_per_page = 10000
-
-        # Obtains items from json
-        filters = request.get_json().items()
-
-        # Filters in verified seisms
-        verified_seisms = db.session.query(SeismModel).filter(SeismModel.verified == True)
-
-        for (key, value) in filters:
-
-            # Page settings from json
-            if key == "page_num":
-                page_num = int(value)
-            if key == "elem_per_page":
-                elem_per_page = int(value)
-
-            # Filters: datetime, magnitude, sensor.name
-            if key == "datetime":
-                verified_seisms = verified_seisms.filter(SeismModel.datetime.like("%" + str(value) + "%"))
-            if key == "magnitude":
-                verified_seisms = verified_seisms.filter(SeismModel.magnitude.like("%" + str(value) + "%"))
-            if key == "sensor.name":
-                verified_seisms = verified_seisms.join(SeismModel.sensor).filter(
-                    SensorModel.name.like("%" + str(value) + "%"))
-
-            # Sorting: datetime (descendant, ascendant), sensor.name (descendant, ascendant)
-            if key == "sort_by":
-                if value == "datetime[desc]":
-                    verified_seisms = verified_seisms.order_by(SeismModel.datetime.desc())
-                if value == "datetime[asc]":
-                    verified_seisms = verified_seisms.order_by(SeismModel.datetime.asc())
-                if value == "sensor.name[desc]":
-                    verified_seisms = verified_seisms.join(SeismModel.sensor).order_by(SensorModel.name.desc())
-                if value == "sensor.name[asc]":
-                    verified_seisms = verified_seisms.join(SeismModel.sensor).order_by(SensorModel.name.asc())
-
-        # Paginates the filtered result and returns a Paginate object
-        verified_seisms = verified_seisms.paginate(page_num, elem_per_page, raise_error, max_elem_per_page)
-        return jsonify({'verified_seisms': [seism_schema.dump(verified_seism) for verified_seism in
-                                            verified_seisms.items]})"""
-
     @admin_login_required
     def post(self):
         seism = SeismModel(
@@ -162,51 +117,25 @@ class UnverifiedSeisms(Resource):
 
     @jwt_required
     def get(self):
+        page_number = 1
+        elem_per_page = 10
 
         # We obtain the user's identity and the JWT claims. We filter the seisms for assigned for the logged user
         user_id = int(get_jwt_identity())
         claims = get_jwt_claims()
 
-        page_num = 1
-        elem_per_page = 10
-        raise_error = True
-        max_elem_per_page = 50
-
-        # Obtains items from json
-        filters = request.get_json().items()
-
         # Filters in unverified_seisms
-        unverified_seisms = db.session.query(SeismModel).filter(SeismModel.verified == False)
+        query = db.session.query(SeismModel).filter(SeismModel.verified == False)
 
         if not claims['admin']:
             # Filters the left associated seisms with the seismologist
-            unverified_seisms = unverified_seisms.filter(SensorModel.user_id == user_id)
+            query = query.filter(SensorModel.user_id == user_id)
 
-        for key, value in filters:
-
-            # Page settings from json
-            if key == "page_num":
-                page_num = int(value)
-            if key == "elem_per_page":
-                elem_per_page = int(value)
-
-            # Filters: sensor_id
-            if key == "sensor_id":
-                unverified_seisms = unverified_seisms.filter(SeismModel.sensor_id.like("%" + str(value) + "%"))
-
-            # Sorting: datetime (descendant, ascendant)
-            if key == "sort_by":
-
-                # Sort by datetime
-                if value == "datetime[desc]":
-                    unverified_seisms = unverified_seisms.order_by(SeismModel.datetime.desc())
-                if value == "datetime[asc]":
-                    unverified_seisms = unverified_seisms.order_by(SeismModel.datetime.asc())
-
-        unverified_seisms = unverified_seisms.paginate(page_num, elem_per_page, raise_error, max_elem_per_page)
-
-        return jsonify({'unverified_seisms': [seism_schema.dump(unverified_seism) for unverified_seism in
-                                              unverified_seisms.items]})
+        pag = PaginationResource(query, page_number, elem_per_page)
+        for key, value in request.get_json().items():
+            query = pag.apply(key, value)
+        query, pagination = pag.pagination()
+        return seisms_schema.dump(query.all())
 
     @admin_login_required
     def post(self):
